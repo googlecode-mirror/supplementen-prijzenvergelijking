87d4a6fd926e3b556ef0db7463c73d1faac3aadc
tuplad@gmail.com <tuplad@gmail.com@db006a05-2225-04a9-ab99-188e99d74c1e>
2011-05-28 23:26:46 +0000

    Huge update.

--- trunk/supplementen-prijzenvergelijking.php
+++ trunk/supplementen-prijzenvergelijking.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
- *      supps.php
+ *      supplementen-prijzenvergelijking.php
  *      
- *      Copyright 2011 Vlad Polianskii <tuplad@gmail.com>
+ *      Copyright 2011 Tuplad <tuplad@gmail.com>
  *      
  *      This program is free software; you can redistribute it and/or modify
  *      it under the terms of the GNU General Public License as published by
@@ -18,100 +19,146 @@
  *      along with this program; if not, write to the Free Software
  *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *      MA 02110-1301, USA.
- */
-?>
+ *
+ * 
+ * ***********************************************
+ *              Variabelen              *
+ *                  ----------        *
+ *     $wp22kg    = Whey Perfection 2,2kg        *
+ *     $wp45kg    = Whey Perfection 4,5kg        *
+ *     $wd1kg    = Whey Delicious 1,0kg          *
+ *     $wd25kg    = Whey Delicious 2,5kg        *
+ *     $sf2kg    = Iron Whey 2,0kg        *
+ *     $bl2kg    = Whey Pro 2,0kg        *
+ *                         *
+ * ********************************************** */
+// config
+$sqlhost = "127.0.0.1";
+$sqluser = "root";
+$sqlpass = "";
+$sqldb = "db_school";
+
+// nodig om te scrapen
+include ("simple_html_dom.php");
+
+echo <<<END
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<title>Supplementen Prijzenophaling</title>
-	</head>
-	<body>
-		<?php
-		// nodig om te scrapen
-		include ("simple_html_dom.php");
-		// verbinden met database
-		// MySQL connectie
-		$mysql_server = 'localhost';
-		$mysql_user = 'root';
-		$mysql_pass = '';
-		$mysql_db = 'db_school';
-		// Eventueel foutmelding
-		@mysql_connect($mysql_server, $mysql_user, $mysql_pass) or die("Er kon geen verbinding worden gemaakt met de MySQL database.");
-		@mysql_select_db($mysql_db) or die("De database kon niet geselecteerd worden!");
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <title>Supplementen Prijzenophaling</title>
+    </head>
+    <body>
+        <h1>Prijzen Vergelijking Demo</h1>
+        <form name="action" action="supplementen-prijzenvergelijking.php" method="POST">
+            <button type="submit" name="update" value="update">Update!</button>
+            <button type="submit" name="view" value="view">View!</button>
+        </form>
+END;
+if (!$_POST['view'] || !$_POST['update']) {
+    echo "<p>Kies een actie</p>";
+} elseif ($_POST['update']) {
+    try {
 
-		/************************************************
-		 *					Variabelen					*
-		 * 					----------					*
-		 * 	$wp22kg	= Whey Perfection 2,2kg				*
-		 * 	$wp45kg	= Whey Perfection 4,5kg				*
-		 * 	$wd1kg	= Whey Delicious 1,0kg				*
-		 * 	$wd25kg	= Whey Delicious 2,5kg				*
-		 * 	$sf2kg	= Iron Whey 2,0kg					*
-		 *	$bl2kg	= Whey Pro 2,0kg					*
-		 * 												*
-		 * 												*
-		 ************************************************/
+        // functie om SQL inserting te vergemakkelijken
+        function addToDB($id, $producent, $product, $gewicht, $prijs) {
+            // variabels global maken om ze in functie te gebruiken
+            global $sqlhost, $sqluser, $sqlpass, $sqldb;
 
-		// DOM object creeëren
-		$html = new simple_html_dom();
+            // verbinden met database
+            $oPDO = new PDO('mysql:host=' . $sqlhost . ';dbname=' . $sqldb . '', $sqluser, $sqlpass);
+            // error handling
+            $oPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		// HTML ophalen Bodyenfitshop
-		$html -> load_file('http://bodyenfitshop.nl/whey-proteine/body-fit-sportsnutrition/whey-perfection');
+            // query klaarmaken en escapen dmv prepare
+            $oResult = $oPDO->prepare("INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (:id, :producent, :product, :gewicht, :prijs) ON DUPLICATE KEY UPDATE D_prijs=VALUES(D_prijs)");
 
-		// Prijs ophalen van B&FShop Whey Perfection dmv <td class=r>
-		// 2,2kg whey perfection
-		$wp22kg = $html -> find(".r", 2) -> innertext;
-		// in mysql
-		$sql = "INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (1, \'Bodyenfitshop\', \'Whey Perfection\', \'2,2kg\', '".$wp22kg."');";
-		// 4,4kg whey perfection
-		$wp45kg = $html -> find(".r", 4) -> innertext;
-		// in mysql
-		$sql = "INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (2, \'Bodyenfitshop\', \'Whey Perfection\', \'4,5kg\', '".$wp45kg."');";
+            // resultaat weergeven
+            if ($oResult) {
+                echo("<p>Query voor " . $producent . " / " . $product . " / " . $gewicht . " uitgevoerd!</p> \n");
+            } else {
+                echo("<p>Geen nieuwe queries uitgevoerd!</p>");
+            }
 
-		// HTML ophalen XXL Nutrition
-		$html -> load_file('http://www.xxlnutrition.nl/whey-delicious/xxl-nutrition');
+            // vars binden
+            $oResult->bindParam(':id', $id);
+            $oResult->bindParam(':producent', $producent);
+            $oResult->bindParam(':product', $product);
+            $oResult->bindParam(':gewicht', $gewicht);
+            $oResult->bindParam(':prijs', $prijs);
 
-		// Gewicht er uit halen, die schrijven wij er zelf bij
-		$pattern = array("/1000 gram &raquo; /", "/2500 gram &raquo; /");
-		// array maken voor info die wij er uit gaan laten
+            // query uitvoeren
+            $oResult->execute();
+        }
 
-		$html = preg_replace($pattern, '', $html);
-		// $pattern vervangen door niks
-		$html = str_get_html($html);
-		// DOM object maken van een string
+        // DOM object creeëren
+        $html = new simple_html_dom();
 
-		// Prijs ophalen van XXL Nutrition Whey Delicious dmv <option value=*>
-		// 1kg whey delicious
-		$wd1kg = $html -> find('option[value=435]', 0) -> innertext;
-		// in mysql
-		$sql = "INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (3, \'XXL Nutrition\', \'Whey Delicious\', \'1,0kg\', '".$wd1kg."');";
-		// 2,5kg whey delicious
-		$wd25kg = $html -> find('option[value=437]', 0) -> innertext;
-		// in mysql
-		$sql = "INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (4, \'XXL Nutrition\', \'Whey Delicious\', \'2,5kg\', '".$wd25kg."');";
+        // HTML ophalen Bodyenfitshop - Whey Perfection
+        $html->load_file('http://bodyenfitshop.nl/whey-proteine/body-fit-sportsnutrition/whey-perfection');
 
-		// HTML ophalen Sportsfood
-		$html -> load_file('http://www.sportfood.nl/shop/product_info.php?products_id=373');
+        // Prijs ophalen van B&FShop Whey Perfection dmv <td class=r>
+        // 2,2kg Whey Perfection
+        $wp22kg = $html->find(".r", 2)->innertext;
+        // in db steken
+        addToDB('1', 'Bodyenfitshop', 'Whey Perfection', '2,2kg', $wp22kg);
+        // 4,5kg Whey Perfection
+        $wp45kg = $html->find(".r", 4)->innertext;
+        // in db steken
+        addToDB('2', 'Bodyenfitshop', 'Whey Perfection', '4,5kg', $wp45kg);
 
-		// Prijs ophalen van Sportsfood Iron Whey dmv <td class=pageHeading>
-		// 2kg iron whey
-		$sf2kg = $html -> find('td[class=pageHeading]', 1) -> innertext;
-		// in mysql
-		$sql = "INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (5, \'Sportfood\', \'Iron Whey\', \'2,0kg\', '".$sf2kg."');";
+        // HTML ophalen XXL Nutrition
+        $html->load_file('http://www.xxlnutrition.nl/whey-delicious/xxl-nutrition');
 
-		// HTML ophalen Bodylab
-		$html -> load_file('http://www.bodylab.nl/?pid=483');
+        // Gewicht er uit halen, die schrijven wij er zelf bij
+        $pattern = array("/1000 gram &raquo; /", "/2500 gram &raquo; /");
+        // array maken voor info die wij er uit gaan laten
 
-		// Prijs ophalen van Bodylab Whey Pro dmv class .rbprice
-		// 2kg whey pro
-		$bl2kg = $html -> find('.rbprice', 0) -> innertext;
-		// in mysql
-		$sql = "INSERT INTO T_supplementen (D_id, D_producent, D_product, D_gewicht, D_prijs) VALUES (6, \'Bodylab\', \'Whey Pro\', \'2,0kg\', '".$bl2kg."');";
-				
-		// geheugen vrijmaken
-			$html->clear();
-			unset($html);
-		?>
-	</body>
+        $html = preg_replace($pattern, '', $html);
+        // $pattern vervangen door niks
+        $html = str_get_html($html);
+        // DOM object maken van een string
+        // Prijs ophalen van XXL Nutrition Whey Delicious dmv <option value=*>
+        // 1kg whey delicious
+        $wd1kg = $html->find('option[value=435]', 0)->innertext;
+        // in mysql
+        addToDB('3', 'XXL Nutrition', 'Whey Delicious', '1,0kg', $wd1kg);
+        // 2,5kg whey delicious
+        $wd25kg = $html->find('option[value=437]', 0)->innertext;
+        // in mysql
+        addToDB('4', 'XXL Nutrition', 'Whey Delicious', '2,5kg', $wd25kg);
+
+        // HTML ophalen Sportfood
+        $html->load_file('http://www.sportfood.nl/shop/product_info.php?products_id=373');
+
+        // Prijs ophalen van Sportfood Iron Whey dmv <td class=pageHeading>
+        // 2kg iron whey
+        $sf2kg = $html->find('td[class=pageHeading]', 1)->innertext;
+        // in mysql
+        addToDB('5', 'Sportfood', 'Iron Whey', '2,0kg', $sf2kg);
+
+        // HTML ophalen Bodylab
+        $html->load_file('http://www.bodylab.nl/?pid=483');
+
+        // Prijs ophalen van Bodylab Whey Pro dmv class .rbprice
+        // 2kg whey pro
+        $bl2kg = $html->find('.rbprice', 0)->innertext;
+        // in mysql
+        addToDB('6', 'Bodylab', 'Whey Pro', '2,0kg', $bl2kg);
+    } catch (PDOException $e) {
+        echo '<pre>';
+        echo 'Regelnummer: ' . $e->getLine() . '<br>';
+        echo 'Bestand: ' . $e->getFile() . '<br>';
+        echo 'Foutmelding: ' . $e->getMessage() . '<br>';
+        echo '</pre>';
+    }
+} elseif ($_POST['view']) {
+    echo <<<END
+    Hello hello!
+END;
+}
+echo <<<END
+    </body>
 </html>
+END;
+?>
